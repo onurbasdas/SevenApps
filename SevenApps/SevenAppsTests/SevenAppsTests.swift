1887a9d467a5a6d2fc7f736cf9bf6dc62720065b
onur <>
2025-01-18 12:28:09 +0300

    Project created.

---
+++ SevenApps/SevenAppsTests/SevenAppsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SevenAppsTests.swift
+//  SevenAppsTests
+//
+//  Created by Metin on 18.01.2025.
+//
+
+import Testing
+@testable import SevenApps
+
+struct SevenAppsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
